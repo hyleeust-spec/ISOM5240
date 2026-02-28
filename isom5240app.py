cdbc7dac55713f34b5858e480a4a5a12d2a14870
Brian Lee <hyleeust@gmail.com>
2026-02-28 17:48:08 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -12,16 +12,17 @@ def age_classifier(image_file_name, models_name):
   
   image_name = image_file_name
   image_name = Image.open(image_name).convert("RGB")
-  
-  age_predictions = age_classifier(image_name)
-  st.write(age_predictions)
-  age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+
 
 def main():
   # Streamlit UI
   st.header("Title: Age Classification using ViT")
   
   age_classifier("middleagedMan.jpg", "prithivMLmods/Age-Classification-SigLIP2")
+  
+  age_predictions = age_classifier(image_name)
+  st.write(age_predictions)
+  age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
   
   # Display results
   st.write("Predicted Age Range:")
