3ff353655530140b2ccc4d92ef67523b4d9f430d
Brian Lee <hyleeust@gmail.com>
2026-02-28 17:51:19 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,3 +1,4 @@
+
 # import part
 from transformers import pipeline
 from PIL import Image
@@ -8,11 +9,11 @@ import streamlit as st
 def ageClassifier(imgFilename, modelName):
     # Load the age classification pipeline
     # The code below should be placed in the main part of the program
-    age_classifier = pipeline(""image-classification"", 
+    age_classifier = pipeline("image-classification", 
                               model=modelName)
     
     image_name = imgFilename
-    image_name = Image.open(image_name).convert(""RGB"")
+    image_name = Image.open(image_name).convert("RGB")
     
     # Classify age
     age_predictions = age_classifier(image_name)
@@ -25,23 +26,23 @@ def output_msg(age_predictions):
     age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
     
     # Display results
-    st.write(""Predicted Age Range:"")
-    st.write(f""Age range: {age_predictions[0]['label']}"")
+    st.write("Predicted Age Range:")
+    st.write(f"Age range: {age_predictions[0]['label']}")
     
-    st.write(""Done"")    
+    st.write("Done")    
 
 
 
 def main():
     # Streamlit UI
-    st.header(""Title: Age Classification using ViT"")
+    st.header("Title: Age Classification using ViT")
     
 
 
-#    age_predictions = ageClassifier(""middleagedMan.jpg"",
-#                                   ""prithivMLmods/Age-Classification-SigLIP2"")
-    age_predictions = ageClassifier(""middleagedMan.jpg"",
-                                   ""akashmaggon/vit-base-age-classification"")
+#    age_predictions = ageClassifier("middleagedMan.jpg",
+#                                   "prithivMLmods/Age-Classification-SigLIP2")
+    age_predictions = ageClassifier("middleagedMan.jpg",
+                                   "akashmaggon/vit-base-age-classification")
     output_msg(age_predictions)
 
   
@@ -49,5 +50,5 @@ def main():
 
 
 # main part
-if __name__ == ""__main__"":
+if __name__ == "__main__":
     main()
