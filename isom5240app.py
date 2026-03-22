df78cb63be4365fb7c2355f12b463c22044ecaee
Brian Lee <hyleeust@gmail.com>
2026-03-22 20:29:25 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -295,7 +295,7 @@ def main():
             )
 
         st.subheader("Tesla Model Detection")
-        st.write(f"Detected model: {result['detected_model]}")
+        st.write(f"Detected model: {result['detected_model']}")
 
         if not result["available_years"]:
             st.warning(f"No available years found in the resale file for {result['detected_model']}.")
